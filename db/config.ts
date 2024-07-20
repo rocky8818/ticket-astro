dbb98537e90236f2ca210296f57b025d1e9313d9
rocky8818 <rickyglz.1318@gmail.com>
2024-07-20 12:50:41 -0600

    🚧 - if you login you can see all the reports ando avery report individually

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,7 @@
 import { column, defineDb, defineTable } from 'astro:db';
 
 const tickets = defineTable({
+
   columns:{
     _id: column.number({ primaryKey: true}),
     ticketNumber: column.text(),
@@ -9,9 +10,28 @@ const tickets = defineTable({
   }
 })
 
+const User = defineTable({
+  columns:{
+    id: column.text({primaryKey: true, optional: false, unique: true}),
+    username: column.text(),
+    password: column.text()
+  }
+})
+
+const Session = defineTable({
+  columns:{
+    id: column.text({optional: false, unique: true}),
+    userId: column.text({ optional:false, references: () => User.columns.id
+    }),
+    expiresAt: column.number({ optional: false}),
+  }
+})
+
 // https://astro.build/db/config
 export default defineDb({
   tables: {
-    tickets
+    tickets,
+    User,
+    Session
   }
 });
